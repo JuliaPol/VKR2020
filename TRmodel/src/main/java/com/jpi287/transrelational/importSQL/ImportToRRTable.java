836467e56f2efa3e2ce62a2634d19bf3d31c32ee
Yulia Polushina <287jpi@gmail.com>
2020-05-02 18:59:22 +0300

    rrt convert

--- TRmodel/src/main/java/com/jpi287/transrelational/importSQL/ImportToRRTable.java
+++ TRmodel/src/main/java/com/jpi287/transrelational/importSQL/ImportToRRTable.java
@@ -1,9 +1,16 @@
 package com.jpi287.transrelational.importSQL;
 
+import com.jpi287.transrelational.table.Cell;
+import com.jpi287.transrelational.table.Column;
 import com.jpi287.transrelational.table.Table;
+import com.jpi287.transrelational.table.rrt.RecordReconstructionColumn;
 import com.jpi287.transrelational.table.rrt.RecordReconstructionTable;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @AllArgsConstructor
@@ -12,15 +19,58 @@ public class ImportToRRTable {
     private final RecordReconstructionTable recordReconstructionTable;
 
     public RecordReconstructionTable convertToRecordReconstructionTable(Table sortedRelationalTable) {
-        return null;
+        List<RecordReconstructionColumn> inversePermutationColumns = sortedRelationalTable.getColumns().stream()
+                .map(this::mapColumn).collect(Collectors.toList());
+        recordReconstructionTable.setColumns(buildRRColumnsFromInversePermutationColumns(inversePermutationColumns));
+        return recordReconstructionTable;
+    }
+
+    private RecordReconstructionColumn mapColumn(Column column) {
+        RecordReconstructionColumn recordReconstructionColumn = new RecordReconstructionColumn();
+        recordReconstructionColumn.setName(column.getName());
+        recordReconstructionColumn.setColumnId(column.getColumnId());
+        int[] permutation = column.getCells().stream().mapToInt(Cell::getPositionInColumn).toArray();
+        recordReconstructionColumn.setCells(getInversePermutation(permutation));
+        return recordReconstructionColumn;
     }
 
     // for permutations started from 1
-    public int[] getInversePermutation(int[] permutation) {
+    int[] getInversePermutation(int[] permutation) {
         int[] inversePermutation = new int[permutation.length];
         for (int i = 1; i <= permutation.length; i++) {
             inversePermutation[permutation[i - 1] - 1] = i;
         }
         return inversePermutation;
+    }
+
+    private List<RecordReconstructionColumn> buildRRColumnsFromInversePermutationColumns(List<RecordReconstructionColumn> inversePermutationColumns) {
+        List<RecordReconstructionColumn> finalColumns = new ArrayList<>();
+        for (int i = 0; i < inversePermutationColumns.size(); i++) {
+            RecordReconstructionColumn inversePermutation = inversePermutationColumns.get(i);
+            RecordReconstructionColumn recordReconstructionColumn = mapColumnFromInversePermutation(inversePermutation);
+            for (int j = 0; j < inversePermutation.getCells().length; j++) {
+                int value = inversePermutation.getCells()[j];
+                int nextColumn = i == inversePermutationColumns.size() - 1 ? 0 : i + 1;
+                int valueFromRightColumn = inversePermutationColumns.get(nextColumn).getCells()[j];
+                recordReconstructionColumn.getCells()[value - 1] = valueFromRightColumn;
+            }
+            finalColumns.add(recordReconstructionColumn);
+        }
+        return finalColumns;
+    }
+
+    private RecordReconstructionColumn mapColumnFromInversePermutation(RecordReconstructionColumn column) {
+        RecordReconstructionColumn recordReconstructionColumn = new RecordReconstructionColumn();
+        recordReconstructionColumn.setName(column.getName());
+        recordReconstructionColumn.setColumnId(column.getColumnId());
+        int[] cells = new int[column.getCells().length];
+        recordReconstructionColumn.setCells(cells);
+        return recordReconstructionColumn;
+    }
+
+    public static void main(String[] args) {
+        int[] permutation = {4, 3, 2, 5, 1};
+        int[] p = (new ImportToRRTable(null)).getInversePermutation(permutation);
+        System.out.println(p);
     }
 }
--- TRmodel/src/test/java/com/jpi287/transrelational/importSQL/ImportToRRTableTest.java
+++ TRmodel/src/test/java/com/jpi287/transrelational/importSQL/ImportToRRTableTest.java
@@ -1,11 +1,16 @@
 package com.jpi287.transrelational.importSQL;
 
+import com.jpi287.transrelational.table.Cell;
+import com.jpi287.transrelational.table.Column;
 import com.jpi287.transrelational.table.Table;
+import com.jpi287.transrelational.table.rrt.RecordReconstructionColumn;
 import com.jpi287.transrelational.table.rrt.RecordReconstructionTable;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
 
 @RunWith(SpringRunner.class)
 public class ImportToRRTableTest {
@@ -22,5 +27,48 @@ public class ImportToRRTableTest {
         for (int i = 0; i <= permutation.length - 1; i++) {
             Assert.assertEquals(expected[i], inversePermutation[i]);
         }
+    }
+
+    @Test
+    public void testConvertToRecordReconstructionTable(){
+        Table sortedTable = new Table("test");
+        Column column1 = new Column();
+        column1.setCells(Arrays.asList(new Cell("S1", 4),
+                new Cell("S2", 3), new Cell("S3", 5),
+                new Cell("S4", 1), new Cell("S4", 2)));
+
+        Column column2 = new Column();
+        column2.setCells(Arrays.asList(new Cell("Adams", 2),
+                new Cell("Blake", 5), new Cell("Clark", 1),
+                new Cell("Jones", 3), new Cell("Smith", 4)));
+
+        Column column3 = new Column();
+        column3.setCells(Arrays.asList(new Cell("10", 3),
+                new Cell("20", 1), new Cell("20", 4),
+                new Cell("30", 2), new Cell("30", 5)));
+
+        Column column4 = new Column();
+        column4.setCells(Arrays.asList(new Cell("Athens", 2),
+                new Cell("London", 1), new Cell("London", 4),
+                new Cell("Paris", 3), new Cell("Paris", 5)));
+        sortedTable.setColumns(Arrays.asList(column1, column2, column3, column4));
+
+
+        RecordReconstructionTable expectedTable = new RecordReconstructionTable("recordReconstructionTable");
+        RecordReconstructionColumn rColumn1 = new RecordReconstructionColumn();
+        rColumn1.setCells(new int[]{5, 4, 2, 3, 1});
+
+        RecordReconstructionColumn rColumn2 = new RecordReconstructionColumn();
+        rColumn2.setCells(new int[]{4, 5, 2, 1, 3});
+
+        RecordReconstructionColumn rColumn3 = new RecordReconstructionColumn();
+        rColumn3.setCells(new int[]{4, 2, 3, 1, 5});
+
+        RecordReconstructionColumn rColumn4 = new RecordReconstructionColumn();
+        rColumn4.setCells(new int[]{5, 4, 1, 2, 3});
+        expectedTable.setColumns(Arrays.asList(rColumn1, rColumn2, rColumn3, rColumn4));
+
+        RecordReconstructionTable actualTable = importToRRTable.convertToRecordReconstructionTable(sortedTable);
+        Assert.assertEquals(expectedTable, actualTable);
     }
 }
